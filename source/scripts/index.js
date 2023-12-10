074761b2f5649b628223129c8ed6a6c6acb07c68
Mikhail <65595208+mikhail7mz@users.noreply.github.com>
2023-12-10 09:55:31 +0300

    вёртска страницы без формы и футера

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,6 @@
 /* в этот файл добавляет скрипты*/
+import {initHeaderMenuDropdown} from './header-menu-dropdown.js';
+import {initHeroSlider} from './hero-slider.js';
+
+initHeaderMenuDropdown();
+initHeroSlider();
